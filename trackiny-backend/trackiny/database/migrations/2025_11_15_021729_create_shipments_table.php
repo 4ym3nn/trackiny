3b083aa047d75b7222f56b89ca7f7279362c8764
Aymen-drid <aymnehocam@gmail.com>
2025-11-16 16:29:48 +0100

    feat(migration): update shipments table schema

--- trackiny-backend/trackiny/database/migrations/2025_11_15_021729_create_shipments_table.php
+++ trackiny-backend/trackiny/database/migrations/2025_11_15_021729_create_shipments_table.php
@@ -12,11 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('shipments', function (Blueprint $table) {
-            $table->shipment_id();
+            $table->id();
             $table->timestamps();
             $table->foreignId('company_id');
             $table->foreignId('transport_id');
-            $table->string('tracking_number');
             $table->string('origin_address');
             $table->string('destination_address');
             $table->dateTime('pickup_date');
