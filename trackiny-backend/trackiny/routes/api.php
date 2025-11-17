05582856567809814221424ab21f59e59006da63
Aymen-drid <aymnehocam@gmail.com>
2025-11-17 20:14:51 +0100

    Update API routes for shipments and transports endpoints

--- trackiny-backend/trackiny/routes/api.php
+++ trackiny-backend/trackiny/routes/api.php
@@ -3,12 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TransportController;
+use App\Http\Controllers\ShipmentController;
 
 
 Route::post('/login',[UserController::class,'login']);
 Route::post('/register',[UserController::class,'register']);
 Route::post('/logout',[UserController::class,'logout']);
-Route::post('/transport/add_shipment',[TransportController::class,'addShipment']);
+Route::post('/shipment/add_shipment',[ShipmentController::class,'addShipment']);
+Route::patch('/shipment/update_shipment/{shipment_id}',[ShipmentController::class,'updateShipment']);
+Route::get('/shipment/get_shipment/{shipment_id}',[ShipmentController::class,'getShipment']);
+Route::get('/shipment/get_shipments/{transport_id}',[ShipmentController::class,'getShipments']);
+
+
 
 
 
