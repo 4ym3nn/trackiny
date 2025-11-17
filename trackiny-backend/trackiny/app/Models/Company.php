315743f1a4478812035ec01c6c2385722a1db1e3
Aymen-drid <aymnehocam@gmail.com>
2025-11-17 17:20:53 +0100

    Update Company model: fillable and relationships

--- trackiny-backend/trackiny/app/Models/Company.php
+++ trackiny-backend/trackiny/app/Models/Company.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\User;
 
+use App\Model\Shipment;
 class Company extends Model
 {
-    const $primaryKey='company_id';
+    protected $fillable = [
+        'user_id',
+        'company_name',
+        'registration_number',
+        'address',
+        'phone',
+        'contact_person',
+    ];
+
+     public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+    public function shipment():BelongsToMany
+    {
+        return $this->belongsToMany(Shipment::class);
+    }
+
 }
+
+
