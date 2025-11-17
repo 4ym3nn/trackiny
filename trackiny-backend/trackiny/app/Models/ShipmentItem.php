413ff88467936937a0e5d57f31691790732584b5
Aymen-drid <aymnehocam@gmail.com>
2025-11-17 17:20:53 +0100

    Update ShipmentItem model: fillable and shipment relation

--- trackiny-backend/trackiny/app/Models/ShipmentItem.php
+++ trackiny-backend/trackiny/app/Models/ShipmentItem.php
@@ -1,9 +1,27 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Model\Shipment;
 class ShipmentItem extends Model
 {
+    protected $fillable = [
+        'shipment_id',
+        'item_name',
+        'description',
+        'quantity',
+        'weight',
+        'unit',
+        'value',
+        'sku',
+        'created_at',
+    ];
+    public $timestamps = false;
+
+    public function shipment():BelongsTo
+             {
+        return $this->belongsTo(Shipment::class,'shipment_id','id');
+
+             }
 }
+
