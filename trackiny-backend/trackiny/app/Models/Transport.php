b3b6af0ffd457c5aa674a74e6117652b00c365c6
Aymen-drid <aymnehocam@gmail.com>
2025-11-17 17:20:53 +0100

    Update Transport model: fillable and relationships

--- trackiny-backend/trackiny/app/Models/Transport.php
+++ trackiny-backend/trackiny/app/Models/Transport.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\User;
+use App\Model\Shipment;
 
 class Transport extends Model
 {
-    protected $primaryKey = 'transport_id';
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+    public function shipment():BelongsToMany
+    {
+        return $this->belongsToMany(Shipment::class,'shipment_id','id');
+    }
+
+    protected $fillable = [
+        'user_id',
+        'company_name',
+        'registration_number',
+        'fleet_size',
+        'license_number',
+        'address',
+        'phone',
+        'contact_person',
+    ];
 }
+
