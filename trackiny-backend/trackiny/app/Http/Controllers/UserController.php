dccabf6f79ba8ff97fc406447b66f5519fe12636
Aymen-drid <aymnehocam@gmail.com>
2025-11-19 19:16:38 +0100

    Update UserController: validation/updates

--- trackiny-backend/trackiny/app/Http/Controllers/UserController.php
+++ trackiny-backend/trackiny/app/Http/Controllers/UserController.php
@@ -4,88 +4,30 @@ namespace App\Http\Controllers;
 
 use App\Models\Company;
 use App\Models\Transport;
+use App\Models\Shipment;
 use App\Models\User;
 use Illuminate\Validation\Rule;
 
-enum UserType: string
-{
-    case COMPANY = 'company';
-    case TRANSPORT = 'transport';
-}
 class UserController extends Controller
 {
-    public function isTransport()
-    {
-        return $this->role === 'transport';
-    }
-
-    public function isCompany()
-    {
-        return $this->role === 'company';
-    }
-
-    public function login()
-    {
-        $incomingFields = request()->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-        if (auth()->attempt($incomingFields)) {
-            session()->regenerate();
-
-            return response()->json([
-                'message' => 'User logged in succesfully',
-            ], 201);
-        } else {
-            return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('email');
-        }
-    }
-
-    public function register()
-    {
-        $incomingFields = request()->validate([
-            'name' => ['required', 'min:3', 'max:20', Rule::Unique('users', 'username')],
-            'email' => ['required', 'email', Rule::Unique('users', 'email')],
-            'password' => ['required'],
-            'type' => ['required', Rule::enum(UserType::class)],
-        ]);
-        $incomingFields['password'] = bcrypt($incomingFields['password']);
-        $user = User::create($incomingFields);
-        if ($user->type === 'company') {
-            Company::create([
-                'user_id' => $user->id,
-                'company_name' => '',
-                'registration_number' => '',
-                'address' => '',
-                'phone' => '',
-                'contact_person' => '',
-            ]);
-        } elseif ($user->type === 'transport') {
-            Transport::create([
-                'user_id' => $user->id,
-                'company_name' => '',
-                'license_number' => '',
-                'fleet_size' => 0,
-                'registration_number' => '',
-                'address' => '',
-                'phone' => '',
-                'contact_person' => '',
-            ]);
-        }
-        auth()->login($user);
-
-        return response()->json([
-            'message' => 'User registered succesfully',
-        ], 201);
+    public function getCompanies(){
+        return User::where('type','company')->get();
 
     }
+    public function getTransports(){
+        return User::where('type','transport')->get();
 
-    public function logout()
+    }
+    public function getShipments(string $company_id)
     {
-        $incomingFields = request()->validate([
-            'username' => ['required', 'min:3', 'max:20'],
-            'email' => ['required', 'email'],
-            'password' => ['required'],
-        ]);
+        $shipments= Shipment::where('company_id',$company_id)->get();
+        return $shipments;
+    }
+
+    public function getShipment( string $shipment_id)
+    {
+        // TO DO : authorization
+        $shipment= Shipment::where('id',$shipment_id)->get();
+        return $shipment;
     }
 }
