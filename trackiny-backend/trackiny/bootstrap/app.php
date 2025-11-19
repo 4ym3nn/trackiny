cc0fe9ea56acb98af0240682b0fcb9aba5a6a43c
Aymen-drid <aymnehocam@gmail.com>
2025-11-19 19:16:38 +0100

    Update bootstrap app configuration

--- trackiny-backend/trackiny/bootstrap/app.php
+++ trackiny-backend/trackiny/bootstrap/app.php
@@ -28,6 +28,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->statefulApi();
         $middleware->append(EnsureTokenIsValid::class);
         $middleware->append(ForceJsonResponse::class);
 
