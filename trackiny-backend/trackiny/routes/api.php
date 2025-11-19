c4898194757bbd70f027ea44624f90fe6a0727ea
Aymen-drid <aymnehocam@gmail.com>
2025-11-19 19:16:38 +0100

    Update API routes

--- trackiny-backend/trackiny/routes/api.php
+++ trackiny-backend/trackiny/routes/api.php
@@ -3,18 +3,33 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TransportController;
-use App\Http\Controllers\ShipmentController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\AuthController;
+
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/register',[AuthController::class,'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
 
 
-Route::post('/login',[UserController::class,'login']);
-Route::post('/register',[UserController::class,'register']);
-Route::post('/logout',[UserController::class,'logout']);
-Route::post('/shipment/add_shipment',[ShipmentController::class,'addShipment']);
-Route::patch('/shipment/update_shipment/{shipment_id}',[ShipmentController::class,'updateShipment']);
-Route::get('/shipment/get_shipment/{shipment_id}',[ShipmentController::class,'getShipment']);
-Route::get('/shipment/get_shipments/{transport_id}',[ShipmentController::class,'getShipments']);
+    // User shared shit
+    Route::get('/get_companies',[UserController::class,'getCompanies']);
+    Route::get('/get_transports',[UserController::class,'getTransports']);
+    Route::get('/get_shipment/{shipment_id}',[UserController::class,'getShipment']);
+    Route::get('/get_shipments/{transport_id}',[UserController::class,'getShipments']);
 
 
+    // company shit
+    Route::post('/company/add_shipment',[CompanyController::class,'addShipment']);
+
+    Route::patch('/company/update_shipment/{shipment_id}',[CompanyController::class,'updateShipment']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout',[AuthController::class,'logout']);
+});
 
 
 
