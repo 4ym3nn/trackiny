6a0d2171a44d5b9f2bcee6547fc73ad9602fbd5d
Aymen-drid <aymnehocam@gmail.com>
2025-11-19 23:22:21 +0100

    remove stateful middlware (csrf ) problems

--- trackiny-backend/trackiny/bootstrap/app.php
+++ trackiny-backend/trackiny/bootstrap/app.php
@@ -28,7 +28,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
         $middleware->append(EnsureTokenIsValid::class);
         $middleware->append(ForceJsonResponse::class);
 
